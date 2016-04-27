c8023665b35008e3f0fcb0b05d2516caf01586d7
prof1990 <prof1990@hotmail.com>
2016-04-27 19:28:01 +0200

    initial commit

---
+++ EliteBinds/Properties/MainWindow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected virtual void OnButtonOpenClicked(object sender, EventArgs e){
+		var a = new Gtk.AboutDialog ();
+		a.Show ();
+	}
+
+	protected virtual void OnButtonSaveClicked(object sender, EventArgs e){
+		Console.Beep (1000, 10);
+	}
+	protected void OnSaveClicked (object sender, EventArgs e)
+	{
+		throw new NotImplementedException ();
+	}
+
+
+	protected virtual void OnSaveButtonClicked (object sender, EventArgs e)
+	{
+		throw new NotImplementedException ();
+	}
+}
---
+++ EliteBinds/WindowManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Gtk;
+
+namespace EliteBinds
+{
+	public class WindowManager
+	{
+		private MainWindow _window;
+		public WindowManager (MainWindow window)
+		{
+			_window = window;
+
+
+		}
+
+		private void LoadButton(object sender, EventArgs e){
+			
+		}			
+	}
+}
+
